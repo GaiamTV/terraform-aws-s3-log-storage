8cc5188ac7e101fb206e0878cf64fdac26c8f791
John Onofrey <john.onofrey@gaia.com>
2020-03-09 13:57:38 -0600

    fix(DEV-796): Changing var back to version 11 compatibility

--- main.tf
+++ main.tf
@@ -1,63 +1,63 @@
 module "default_label" {
   source      = "git@github.com:GaiamTV/tf-null-label.git"
-  enabled     = var.enabled
-  namespace   = var.namespace
-  environment = var.environment
-  stage       = var.stage
-  name        = var.name
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
+  enabled     = "${var.enabled}"
+  namespace   = "${var.namespace}"
+  environment = ${var.environment}
+  stage       = "${var.stage}"
+  name        = "${var.name}"
+  delimiter   = "${var.delimiter}"
+  attributes  = "${var.attributes}"
+  tags        = "${var.tags}"
 }
 
 resource "aws_s3_bucket" "default" {
-  count         = var.enabled ? 1 : 0
+  count         = "${var.enabled}" ? 1 : 0
   bucket        = module.default_label.id
-  acl           = var.acl
-  region        = var.region
-  force_destroy = var.force_destroy
-  policy        = var.policy
+  acl           = "${var.acl}"
+  region        = "${var.region}"
+  force_destroy = "${var.force_destroy}"
+  policy        = "${var.policy}"
 
   versioning {
-    enabled = var.versioning_enabled
+    enabled = "${var.versioning_enabled}"
   }
 
   lifecycle_rule {
     id      = module.default_label.id
-    enabled = var.lifecycle_rule_enabled
+    enabled = "${var.lifecycle_rule_enabled}"
 
-    prefix = var.lifecycle_prefix
-    tags   = var.lifecycle_tags
+    prefix = "${var.lifecycle_prefix}"
+    tags   = "${var.lifecycle_tags}"
 
     noncurrent_version_expiration {
-      days = var.noncurrent_version_expiration_days
+      days = "${var.noncurrent_version_expiration_days}"
     }
 
     dynamic "noncurrent_version_transition" {
-      for_each = var.enable_glacier_transition ? [1] : []
+      for_each = "${var.enable_glacier_transition}" ? [1] : []
 
       content {
-        days          = var.noncurrent_version_transition_days
+        days          = "${var.noncurrent_version_transition_days}"
         storage_class = "GLACIER"
       }
     }
 
     transition {
-      days          = var.standard_transition_days
+      days          = "${var.standard_transition_days}"
       storage_class = "STANDARD_IA"
     }
 
     dynamic "transition" {
-      for_each = var.enable_glacier_transition ? [1] : []
+      for_each = "${var.enable_glacier_transition}" ? [1] : []
 
       content {
-        days          = var.glacier_transition_days
+        days          = "${var.glacier_transition_days}"
         storage_class = "GLACIER"
       }
     }
 
     expiration {
-      days = var.expiration_days
+      days = "${var.expiration_days}"
     }
   }
 
@@ -66,8 +66,8 @@ resource "aws_s3_bucket" "default" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = var.sse_algorithm
-        kms_master_key_id = var.kms_master_key_arn
+        sse_algorithm     = "${var.sse_algorithm}"
+        kms_master_key_id = "${var.kms_master_key_arn}"
       }
     }
   }
